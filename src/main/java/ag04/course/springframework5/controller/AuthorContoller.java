09a00162d604c585eeabd5f94bef7495aefa3736
Hana Hrustić <hana.hrustic@gmail.com>
2022-03-09 12:08:31 +0100

    Connect H2 database, configure Spring MVC Controllers and Thymeleaf templating
    (cherry picked from commit 018ada0f3030b8838083772a41375101fe9d2b66)

---
+++ src/main/java/ag04/course/springframework5/controller/AuthorContoller.java
@@ -0,0 +1,26 @@
+package ag04.course.springframework5.controller;
+
+import ag04.course.springframework5.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorContoller {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorContoller(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
