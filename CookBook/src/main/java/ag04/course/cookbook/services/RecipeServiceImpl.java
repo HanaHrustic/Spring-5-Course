1b03d97a0de1c64548a6a66b62f1feebb0e102e8
Hana Hrustić <hana.hrustic@gmail.com>
2022-03-16 13:14:12 +0100

    Refactor with Lambok

--- CookBook/src/main/java/ag04/course/cookbook/services/RecipeServiceImpl.java
+++ CookBook/src/main/java/ag04/course/cookbook/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package ag04.course.cookbook.services;
 
 import ag04.course.cookbook.domain.Recipe;
 import ag04.course.cookbook.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -26,6 +28,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service.");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
--- CookBook/src/main/java/ag04/course/cookbook/services/UnitOfMeasureService.java
+++ CookBook/src/main/java/ag04/course/cookbook/services/UnitOfMeasureService.java
@@ -1,7 +1,6 @@
 package ag04.course.cookbook.services;
 
 import ag04.course.cookbook.domain.UnitOfMeasure;
-import ag04.course.cookbook.repositories.UnitOfMeasureRepository;
 
 public interface UnitOfMeasureService {
     UnitOfMeasure getUnitOfMeasure(String description);
